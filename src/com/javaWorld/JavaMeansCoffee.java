cd482e2b696ffe33a0282ff2e4f1cab435816b0a
AniRavIndira <anithamyoffical@gmail.com>
2022-01-23 10:00:33 +0530

    init commit

---
+++ src/com/javaWorld/JavaMeansCoffee.java
@@ -0,0 +1,27 @@
+package com.javaWorld;
+
+public class JavaMeansCoffee {
+
+	public static void main(String[] args) {
+		String str = "Hello please check the mail";
+		String[] array = str.split(" ");
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (i >= 0) {
+
+				char[] charArray = array[i].toCharArray();
+				char[] charArray1 = new char[charArray.length];
+				int k = 0;
+				for (int j = charArray.length - 1; j >= 0; j--) {
+					charArray1[k] = charArray[j];
+					k++;
+				}
+				String revstring = new String(charArray1);
+			   System.out.print(" "+revstring);
+			} else {
+				System.out.print("Please enter some string..");
+			}
+		}
+	}
+}
