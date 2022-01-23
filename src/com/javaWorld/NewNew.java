ee3773ee713beffb2a9e0e981b7d6d1cc3a86f71
AniRavIndira <anithamyoffical@gmail.com>
2022-01-23 15:17:50 +0530

    please add all

---
+++ src/com/javaWorld/NewNew.java
@@ -0,0 +1,11 @@
+package com.javaWorld;
+
+public class NewNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello please check the mail";
+		String[] array = str.split(" ");
+	}
+
+}
